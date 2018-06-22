9d2522bcd456b1ec19a7cf2289e0d4ee4ade18e0
Jose Martinez <josmmartinez94@gmail.com>
2018-06-21 22:32:44 -0700

    Add stack class to include methods: push, pop, and peek

--- W1D5/stack.rb
+++ W1D5/stack.rb
@@ -1,14 +1,19 @@
 class Stack
-  
+
   def initialize
+    @stack = []
   end
 
   def push(el)
+    @stack << el
   end
 
   def pop
+    @stack.pop
   end
 
   def peek
+    return [] if @stack.length == 0
+    @stack[0]
   end
 end

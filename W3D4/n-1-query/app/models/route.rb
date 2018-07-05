60a3dd0e555af93bc812483544f3f0b0161241f9
Jose Martinez <josmmartinez94@gmail.com>
2018-07-05 07:22:42 -0700

    Add better queries for drivers, seeds, and tracks

--- W3D4/n-1-query/app/models/route.rb
+++ W3D4/n-1-query/app/models/route.rb
@@ -20,6 +20,17 @@ class Route < ApplicationRecord
   end
 
   def better_drivers_query
-    # TODO: your code here
+    buses = self.buses.includes(:drivers)
+
+    all_drivers = {}
+    buses.each do |bus|
+      drivers = []
+      bus.drivers.each do |driver|
+        driver << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+
+    all_drivers
   end
 end
